187665abf84215d0d59eaa3628182705989ff78b
Maylander <alistermaylander@gmail.com>
2024-01-15 12:00:57 +1100

    +

--- task/task.go
+++ task/task.go
@@ -26,6 +26,27 @@ const (
 	Failed
 )
 
+var stateTransitionMap = map[State][]State{
+	Pending:   {Scheduled},
+	Scheduled: {Scheduled, Running, Failed},
+	Running:   {Running, Completed, Failed},
+	Completed: {},
+	Failed:    {},
+}
+
+func Contains(states []State, state State) bool {
+	for _, s := range states {
+		if s == state {
+			return true
+		}
+	}
+	return false
+}
+
+func ValidStateTransition(src State, dst State) bool {
+	return Contains(stateTransitionMap[src], dst)
+}
+
 type Task struct {
 	ID            uuid.UUID
 	ContainerId   string
